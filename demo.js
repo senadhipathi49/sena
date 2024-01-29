da8be4c56315bb576714c9227e851dd65906458e
Senadhipathi <sena572002@gmail.com>
2024-01-29 10:10:34 +0530

    created

---
+++ demo.js
@@ -0,0 +1,7 @@
+<script>
+    // Simulate asynchronous content loading
+    window.addEventListener('load', function () {
+        document.querySelector('.loader-wrapper').style.display = 'none'
+        // Additional code to reveal the loaded content or perform other actions
+    });
+</script>
